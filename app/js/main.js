4e302ae7675998086a6de6542a012784bcc19cba
Julia-Kuldorova <julia.julivne@gmail.com>
2020-07-20 21:04:36 +0300

    added siider

--- app/js/main.js
+++ app/js/main.js
@@ -8,5 +8,15 @@ $(function(){
 	      $('input, select').styler();
     }, 100)
 
-
+    $('.play-slider__inner').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow:
+            '<button class="slick-arrow slick-prev"><img src="../images/icons/arrow-prev.png" alt=""></button>',
+        nextArrow:
+            '<button class="slick-arrow slick-next"><img src="../images/icons/arrow-next.png" alt=""></button>',
+        variableWidth: true
+        // centerMode: true,
+    });
 });
